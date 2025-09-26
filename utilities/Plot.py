5ede11165dd9ac575be8c5e2ecee8616cf49f09a
MraDmr0 <dimaremario24@gmail.com>
2025-09-26 17:39:57 +0200

    Update Plot.py

--- utilities/Plot.py
+++ utilities/Plot.py
@@ -1,43 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 # Percorso del file di testo
-file_path = 'output'
+prefix = str(sys.argv[1])
+file_path = prefix + ".txt"
 
 # Caricamento dati
-# Si presume che i numeri complessi siano nel formato 'a+bj' o 'a-bj'
 dati = np.loadtxt(file_path, dtype=complex)
 
-# Estrazione delle colonne
-t    = dati[:, 0].real         # Prima colonna (reale)
-env  = dati[:, 1].real         # Seconda colonna (reale)
-psi0 = dati[:, 2]              # Terza colonna (complessa)
-psi1 = dati[:, 3]              # Quarta colonna (complessa)
-psi2 = dati[:, 4]              # Quinta colonna (complessa)
-psi3 = dati[:, 5]              # Sesta colonna (complessa)
+# Estrazione colonne
+t = dati[:, 0].real      # Prima colonna (reale)
+env = dati[:, 1].real    # Seconda colonna (reale)
+psis = dati[:, 2:]       # Tutte le colonne successive (stati) come array 2D
 
-fig , ax = plt.subplots(1 , 1 , figsize = (8,8) ,  dpi = 300)
-#append data to the figure
-ax.plot(t , abs(psi0)[:]**2 , label = r"State $|0\rangle$")
-ax.plot(t , abs(psi1)[:]**2 , label = r"State $|1\rangle$")
+fig, ax = plt.subplots(1, 1, figsize=(8, 8), dpi=300)
 
+# Ciclo per plottare tutti gli stati
+for i, psi in enumerate(psis.T):  # psis.T per iterare sulle colonne
+    ax.plot(t, abs(psi)**2, label=fr"State $|{i}\rangle$")
 
-ax.plot(t , abs(psi2)[:]**2 , label = r"State $|2\rangle$")
-ax.plot(t , abs(psi3)[:]**2 , label = r"State $|3\rangle$")
+# Envelope function
+ax.plot(np.nan, ls="--", c="grey", alpha=0.7, label="Envelope function")
+ax.legend(loc=0)
 
-ax.plot(np.nan , ls = "--", c = "grey", alpha = 0.7,  label = "Envelope function" )
-ax.legend(loc = 0)
-
+# Secondo asse y per l'envelope
 ax1 = ax.twinx()
-ax1.plot(t , env , ls = "--", c = "grey", alpha = 0.7)
+ax1.plot(t, env, ls="--", c="grey", alpha=0.7)
 
-#personalization
+# Personalizzazione assi
 ax.set_xlabel(r"Time $(\mu s)$")
 ax.set_ylabel(r"$|\Psi|^2$")
 ax1.set_ylabel("Envelope intensity")
 
-#save fiugre
-plt.savefig("output.png")
-
-
+# Salvataggio figura
+outfile = prefix + ".png"
+plt.savefig(outfile)
 
